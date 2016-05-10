91584a1538b9ec0ba8bf14be90de6333ff6c0a7a
ChinatsuKawakami <zarusoba3939@gmail.com>
2016-05-10 14:14:32 -0400

    HelloWorldConsoleMethod

--- COMP123-S2016-Lesson1/Program.cs
+++ COMP123-S2016-Lesson1/Program.cs
@@ -10,7 +10,19 @@ namespace COMP123_S2016_Lesson1
     {
         static void Main(string[] args)
         {
+            //call the hello workd to console Method
+            HelloWorldToConsole();
+        }      
+        //This is my helloworld method
+        //method HelloworldToConsole
+        //return {string} HelloworldString
+            public static string HelloWorldToConsole()
+            {
+            string HelloWorldString ="Hello,world!";
+                Console.WriteLine(HelloWorldString);
+                return HelloWorldString;
+            }
 
         }
     }
-}
+
